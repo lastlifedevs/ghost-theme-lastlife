79454e0d444d1c051df844e1dae2445c5752277c
Veegie <jbalanuik@gmail.com>
2018-11-21 01:42:58 -0500

    Move creators JS to external asset. Remove self-aggrandizement.

---
+++ assets/js/creators.js
@@ -0,0 +1,19 @@
+$(document).ready((function() {
+    $(".profile-icon").hover(function(){
+        let currentIcon = $(this);
+        let profContainer = $("#profiles-container");
+        let selectedProfile = $("#profile-"+$.escapeSelector($(this).data("prof-id")));
+        selectedProfile.show();
+        let profContainerHeight = profContainer.outerHeight();
+        selectedProfile.hide();
+        let profileLoc = (currentIcon.offset().top - $(window).scrollTop()) + currentIcon.outerHeight();
+        if (profileLoc + profContainerHeight + 100 > $(document).outerHeight()) {
+            profileLoc -= (currentIcon.outerHeight() + profContainerHeight);
+        }
+        profContainer.css("top", profileLoc + "px");
+        $(".creator-profile").stop().hide();
+        selectedProfile.fadeIn();
+    }, function() {
+        $(".creator-profile").hide();
+    });
+}));
