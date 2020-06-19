4d306be913d0ca99fd869b2d9656b9377b0b6f0e
Veegie <jbalanuik@gmail.com>
2020-06-18 21:45:56 -0400

    Finish upgrading theme for compatibility with Ghost 3.0.

--- assets/built/creators.js
+++
@@ -1,2 +0,0 @@
-$(document).ready(function(){$(".profile-icon").hover(function(){let e=$(this),o=$("#profiles-container"),t=$("#profile-"+$.escapeSelector($(this).data("prof-id")));t.show();let i=o.outerHeight();t.hide();let r=e.offset().top-$(window).scrollTop()+e.outerHeight();r+i+100>$(document).outerHeight()&&(r-=e.outerHeight()+i),o.css("top",r+"px"),$(".creator-profile").stop().hide(),t.fadeIn()},function(){$(".creator-profile").hide()})});
-//# sourceMappingURL=creators.js.map
